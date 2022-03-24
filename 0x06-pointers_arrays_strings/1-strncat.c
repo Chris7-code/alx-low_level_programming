d3f6597a46033a027bc9e8579c43ea8a4e97554e
Chris7-code <mbonimpachristian@gmail.com>
2022-03-24 14:36:11 -0700

    1-stncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,30 +1,27 @@
 #include "main.h"
-/** 
- * len - function calculates the length of a given string.
- * 2dest: pointer to destination .
- * Return: Always 0.
- */
-
-int len(char *dest)
-{
-	return ((*dest != '\0') ? 1 + len(dest + 1) : 0);
-}
 /**
  * _strncat - function that concatenates two strings.
- * @dest: the string where we want to append.
- * @src: the string from which 'n' characters are going to append.
- * @n: represents maximum number of character to be appended.
- * Return: dest.
+ * @dest: pointer to destination char
+ * @src: pointer to source char.
+ * @n: number of bytes to recieve
+ * Return: char.
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	const unsigned int log = len(dest);
+	unsigned int i = 0;
+	int j= 0;
 
-	for (i = 0; i < n; i++)
+	while (*(dest + i) != '\0')
+		i++;
+	while (*(src + j) != '\0')
 	{
-		*(dest + i + log) = *(src + i);
+		if(j < n)
+		{
+			*(dest + i) = *(src + j);
+			i++;
+		}
+		j++;
 	}
 	return (dest);
 }
