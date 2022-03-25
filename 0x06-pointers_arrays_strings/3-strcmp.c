d029588532563453737117380198c6f584a8ce47
Chris7-code <mbonimpachristian@gmail.com>
2022-03-25 00:49:47 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,17 +15,17 @@ int _strcmp(char *s1, char *s2)
 
 	while (*(s1 + i) != '\0')
 	{
-		if (*(s1 + i) > *(s2 +i))
+		if (*(s1 + i) > *(s2 + i))
 		{
 			aux = *(s1 + i) - *(s2 + i);
-			return(aux);
+			return (aux);
 		}
 		else if (*(s1 + i) < *(s2 + i))
 		{
 			aux = *(s1 + i) - *(s2 + i);
-			return(aux);
+			return (aux);
 		}
 		i += 1;
 	}
-	return(0);
+	return (0);
 }
