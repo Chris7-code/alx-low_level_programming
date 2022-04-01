a5ae4e9edeb6b6ac186f147165785abf7659445c
Chris7-code <mbonimpachristian@gmail.com>
2022-04-01 05:20:08 -0700

    second commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,65 +5,82 @@
 #include <stdbool.h>
 
 /**
- * coinConverter - Helper function that does all the mathematics
- * @i: Passed in variable from main for calculations
- * Return: The number of coins needed minimum for the passed in variable
- */
-int coinConverter(int i)
-{
-	int count = 0;
-
-	while (i != 0)
-	{
-		if (i % 10 == 9 || i % 10 == 7)
-			i -= 2;
-		else if (i % 25 == 0)
-			i -= 25;
-		else if (i % 10 == 0)
-			i -= 10;
-		else if (i % 5 == 0)
-			i -= 5;
-		else if (i % 2 == 0)
-		{
-			if (i % 10 == 6)
-				i -= 1;
-			else
-				i -= 2;
-		}
-		else
-			i -= 1;
-
-		count++;
-	}
-	return (count);
-}
-
-/**
- * main - Takes in exactly one argument for minimum coin count
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0 if exactly 1 argument i passed into this program, 1 otherwise
+ * main - Print the minimum number of coins to make change
+ * @argc: the number of command line arguments
+ * @argv: The command line arguments
+ *
+ * Return: 0 if successful, 1 if number of arguments is not 1
  */
 int main(int argc, char *argv[])
 {
-	int i, coin;
-
-	coin = 0;
-
+	int cents;
+	
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	i = atoi(argv[1]);
-
-	if (i < 0)
-		printf("0\n");
-	else
+	cents = atoi(argv[1]);
+	if (cents <= 0)
 	{
-		coin = coinConverter(i);
-		printf("%d\n", coin);
+		printf("0\n");
+		return (0);
 	}
+
+	if (!is_positiv_number(argv[1]))
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", number_of_coins(cents));
+	
 	return (0);
+}
+
+/**
+ * is_positive_number - Check if a string contains only digits
+ * @number: The string to check
+ *
+ * Return: 1 if string contains only digits, 0 otherwise
+ */
+
+int is_positive_number(char *number)
+{
+	int i;
+
+	for (i = 0; number[i] != '\0'; i++)
+	{
+		if (!isdigit(numebr[i]))
+			return (0);
+	}
+
+	return (1);
+}
+
+/**
+ * number_of_coins -determine the minimum  number of coins to make change 
+ * @cents: The amount of change
+ *
+ * Return: The minimum number of coins
+ */
+int number_of_coins(int cents)
+{
+	int coins = 0;
+
+	coins += (cents / 25);
+	cents %= 25;
+
+	coins += (cents / 10);
+	cents %= 10;
+
+	coins += (cents / 5);
+	cents %= 5;
+
+	coins += (cents / 2);
+	cents %= 2;
+
+	coins += cents;
+
+	return (coins);
 }
