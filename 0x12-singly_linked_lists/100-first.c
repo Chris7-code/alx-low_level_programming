c35a5351de9acdac2a8e9d6f5890ccd154684d3b
Chris7-code <mbonimpachristian@gmail.com>
2022-04-21 14:22:45 -0700

    0x012-singly_linked_lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-void print_something(void) __attribute__ ((constructor));
-
 /**
  * print_something - prints a msg before the main
  */
-
+void print_something(void) __attribute__ ((constructor));
 void print_something(void)
 {
 	printf("You're beat! and yet, you must allow, \n");
